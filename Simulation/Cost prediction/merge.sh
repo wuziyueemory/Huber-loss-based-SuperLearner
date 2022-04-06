b2633ec131216ebdc72cb57673b98fa4d4acf3b1
wuziyueemory <49380137+wuziyueemory@users.noreply.github.com>
2022-04-05 17:32:07 -0700

    Create merge.sh

---
+++ Simulation/Cost prediction/merge.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "#SBATCH --mail-user=zwu56@emory.edu" >> script.out
+echo "#SBATCH --partition=short-cpu" >> script.out
+echo "#SBATCH --job-name=merge" >> script.out
+echo "#SBATCH --output=/projects/dbenkes/ziyue/topic_2/cost/merge.out" >> script.out
+echo "#SBATCH --error=/projects/dbenkes/ziyue/topic_2/cost/merge.err" >> script.out
+
+module purge
+module load R/4.0.2
+srun R --vanilla  < /projects/dbenkes/ziyue/topic_2/cost/merge.R
